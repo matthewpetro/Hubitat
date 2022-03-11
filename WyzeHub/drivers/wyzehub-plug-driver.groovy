95b267a97abd36a54a2331d5a31fbf648d9cc1c3
Matthew Petro <matthew.petro@aa.com>
2022-03-11 07:51:50 -0700

    Added auto refresh of device state

--- WyzeHub/drivers/wyzehub-plug-driver.groovy
+++ WyzeHub/drivers/wyzehub-plug-driver.groovy
@@ -6,15 +6,15 @@
  * Version 1.1, December 2016
  *
  * Copyright (C) 2021 Jake Lehner
- * 
+ *
  * Everyone is permitted to copy and distribute verbatim or modified
  * copies of this license document.
- * 
+ *
  * DON'T BE A DICK PUBLIC LICENSE
  * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
  *
  * 1. Do whatever you like with the original work, just don't be a dick.
- * 
+ *
  *    Being a dick includes - but is not limited to - the following instances:
  *
  *    1a. Outright copyright infringement - Don't just copy this and change the name.
@@ -24,7 +24,7 @@
  * 2. If you become rich through modifications, related works/services, or supporting the original work,
  *    share the love. Only a dick would make loads off this work and not buy the original work's
  *    creator(s) a pint.
- * 
+ *
  * 3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
  *    you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
  *
@@ -32,7 +32,7 @@
 
 import groovy.transform.Field
 
-public static String version() {  return "v1.4"  }
+public static String version() {  return 'v1.4'  }
 
 public String deviceModel() { return device.getDataValue('product_model') ?: 'WLPP1CFH' }
 
@@ -52,192 +52,153 @@ public String deviceModel() { return device.getDataValue('product_model') ?: 'WL
 @Field static final String wyze_property_device_vacation_mode_value_false = '0'
 
 metadata {
-	definition(
-		name: "WyzeHub Plug", 
-		namespace: "jakelehner", 
-		author: "Jake Lehner", 
-		importUrl: "https://raw.githubusercontent.com/jakelehner/Hubitat/master/WyzeHub/drivers/wyzehub-plug-driver.groovy"
-	) {
-		capability "Outlet"
-		capability "Switch"
-		capability "Refresh"
+  definition(
+    name: 'WyzeHub Plug',
+    namespace: 'jakelehner',
+    author: 'Jake Lehner',
+    importUrl: 'https://raw.githubusercontent.com/jakelehner/Hubitat/master/WyzeHub/drivers/wyzehub-plug-driver.groovy'
+  ) {
+    capability 'Outlet'
+    capability 'Switch'
+    capability 'Refresh'
 
-		attribute "vacationMode", "bool"
-		attribute "online", "bool"
-		attribute "rssi", "number"
-	}
+    attribute 'vacationMode', 'bool'
+    attribute 'online', 'bool'
+    attribute 'rssi', 'number'
+  }
 
-	preferences 
-	{
-		
-	}
+  preferences {
+    input name: 'refreshInterval', type: 'number', title: 'Refresh Interval', description: 'Number of minutes between automatic refreshes of device state. 0 means no automatic refresh', required: false, defaultValue: 5, range: 0..60
+  }
 }
 
 void installed() {
-    log.debug "installed()"
+  log.debug 'installed()'
+  initialize()
+  refresh()
+}
 
-	// TODO Make Configurable
-	unschedule('refresh')
-	//schedule('0/10 * * * * ? *', 'refresh')
-
-    refresh()
-	initialize()
+void uninstalled() {
+  log.debug 'uninstalled()'
+  unschedule('refresh')
 }
 
 void updated() {
-   log.debug "updated()"
-   initialize()
+  log.debug 'updated()'
+  initialize()
 }
 
 void initialize() {
-   log.debug "initialize()"
+  log.debug 'initialize()'
+  unschedule('refresh')
+  if (refreshInterval > 0) {
+    schedule("0 0/${refreshInterval} * * * ? *", 'refresh')
+  }
 }
 
 void parse(String description) {
-	log.warn("Running unimplemented parse for: '${description}'")
+  log.warn("Running unimplemented parse for: '${description}'")
 }
 
 def refresh() {
-	app = getApp()
-	logInfo("Refresh Device")
-	app.apiGetDevicePropertyList(device.deviceNetworkId, deviceModel())
-
-	// TODO Make Configurable
-	// keepFresh = true
-	// keepFreshSeconds = 10
-	// runIn(keepFreshSeconds, 'refresh')
+  app = getApp()
+  logInfo('Refresh Device')
+  app.apiGetDevicePropertyList(device.deviceNetworkId, deviceModel())
 }
 
 def on() {
-	app = getApp()
-	logInfo("'On' Pressed")
+  app = getApp()
+  logInfo("'On' Pressed")
 
-	app.apiSetDeviceProperty(device.deviceNetworkId, deviceModel(), wyze_property_power, wyze_property_power_value_on)
+  app.apiSetDeviceProperty(device.deviceNetworkId, deviceModel(), wyze_property_power, wyze_property_power_value_on)
 }
 
 def off() {
-	app = getApp()
-	logInfo("'Off' Pressed")
+  app = getApp()
+  logInfo("'Off' Pressed")
 
-	app.apiSetDeviceProperty(device.deviceNetworkId, deviceModel(), wyze_property_power, wyze_property_power_value_off)
+  app.apiSetDeviceProperty(device.deviceNetworkId, deviceModel(), wyze_property_power, wyze_property_power_value_off)
 }
 
 void createDeviceEventsFromPropertyList(List propertyList) {
-	app = getApp()
-    logDebug("createEventsFromPropertyList()")
+  app = getApp()
+  logDebug('createEventsFromPropertyList()')
 
-    String eventName, eventUnit
-    def eventValue // could be String or number
+  String eventName, eventUnit
+  def eventValue // could be String or number
 
-    // Feels silly to loop through this twice but we need colorMode early.
-    // TODO Better way to search propertyList for element with pid = P1508?
-    propertyList.each { property ->
-        if(property.pid == wyze_property_color_mode) {
-			
-            deviceColorMode = (property.value == "1" ? 'RGB' : 'CT')
-            
-            if (device.hasCapability('ColorMode')) {
-                eventName = "colorMode"
-                eventUnit = null
-                eventValue = deviceColorMode
-
-				if (device.currentValue(eventName) != eventValue) {
-					logDebug("Updating Property 'colorMode' to ${eventValue}")
-					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
-				}
-            }
+  propertyList.each { property ->
+    propertyValue = property.value ?: property.pvalue ?: null
+    switch (property.pid) {
+      // Switch State
+      case wyze_action_power_on:
+      case wyze_action_power_off:
+      case wyze_property_power:
+        eventName = 'switch'
+        eventUnit = null
+        if (propertyValue == wyze_property_power_value_on || propertyValue == wyze_action_power_on) {
+          eventValue = 'on'
+        } else {
+          eventValue = 'off'
         }
+        break
+
+      // Device Online
+      case wyze_property_device_online:
+        eventName = 'online'
+        eventUnit = null
+        eventValue = propertyValue == wyze_property_device_online_value_true ? 'true' : 'false'
+        break
+
+      // RSSI
+      case wyze_property_rssi:
+        eventName = 'rssi'
+        eventUnit = 'db'
+        eventValue = propertyValue
+        break
+
+      // Vacation Mode
+      case wyze_property_vacation_mode:
+        eventName = 'vacationMode'
+        eventUnit = null
+        eventValue = propertyValue == wyze_property_device_vacation_mode_value_true ? 'true' : 'false'
+        break
     }
+    sendDeviceEvent(app, eventName, eventValue, eventUnit)
+  }
+}
 
-    propertyList.each { property ->
-	
-		propertyValue = property.value ?: property.pvalue ?: null
-		switch(property.pid) {
-            // Switch State
-			case wyze_action_power_on:
-			case wyze_action_power_off:
-            case wyze_property_power:
-				eventName = "switch"
-                eventUnit = null
-
-				if(propertyValue == wyze_property_power_value_on) {
-					eventValue = "on"
-				} else if(propertyValue == wyze_action_power_on) {
-					eventValue = "on"
-				} else {
-					eventValue = "off"
-				}
-
-				if (device.currentValue(eventName) != eventValue) {
-					logDebug("Updating Property 'switch' to ${eventValue}")
-					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
-				}
-            break
-        
-            // Device Online
-            case wyze_property_device_online:
-                eventName = "online"
-                eventUnit = null
-                eventValue = propertyValue == wyze_property_device_online_value_true ? "true" : "false"
-                
-				if (device.currentValue(eventName) != eventValue) {
-					logDebug("Updating Property 'online' to ${eventValue}")
-					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
-				}
-            break
-
-            // RSSI
-            case wyze_property_rssi:
-                eventName = "rssi"
-                eventUnit = 'db'
-                eventValue = propertyValue
-
-				if (device.currentValue(eventName) != eventValue) {
-					logDebug("Updating Property 'rssi' to ${eventValue}")
-					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
-				}
-            break
-
-            // Vacation Mode
-            case wyze_property_vacation_mode:
-                eventName = "vacationMode"
-                eventUnit = null
-                eventValue = propertyValue == wyze_property_device_vacation_mode_value_true ? "true" : "false"
-
-				if (device.currentValue(eventName) != eventValue) {
-					logDebug("Updating Property 'vacationMode' to ${eventValue}")
-					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
-				}
-            break
-
-        }
-    }
+private sendDeviceEvent(app, eventName, eventValue, eventUnit) {
+  if (device.currentValue(eventName) != eventValue) {
+    logDebug("Updating Property '${eventName}' to ${eventValue}")
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+  }
 }
 
 private getApp() {
-	app = getParent()
-	while(app && app.name != "WyzeHub") {
-		app = app.getParent()
-	}
-	return app
+  app = getParent()
+  while (app && app.name != 'WyzeHub') {
+    app = app.getParent()
+  }
+  return app
 }
 
 private void logDebug(message) {
-	app = getApp()
-	app.logDebug("[${device.label}] " + message)
+  app = getApp()
+  app.logDebug("[${device.label}] " + message)
 }
 
 private void logInfo(message) {
-	app = getApp()
-	app.logInfo("[${device.label}] " + message)
+  app = getApp()
+  app.logInfo("[${device.label}] " + message)
 }
 
 private void logWarn(message) {
-	app = getApp()
-	app.logWarn("[${device.label}] " + message)
+  app = getApp()
+  app.logWarn("[${device.label}] " + message)
 }
 
 private void logError(message) {
-	app = getApp()
-	app.logError("[${device.label}] " + message)
+  app = getApp()
+  app.logError("[${device.label}] " + message)
 }
